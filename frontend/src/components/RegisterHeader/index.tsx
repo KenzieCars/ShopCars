a0a70bbe67c459fa1783cb3217455a7143a13651
JoseMartins0808 <ottenazuose@hotmail.com>
2023-08-11 23:02:33 -0300

    feat: fix footer / header links & disable register submit button

---
+++ frontend/src/components/RegisterHeader/index.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { Menu } from "@mui/material";
+import LogoHeader from "../../../public/LogoHeader.png";
+import { useMediaQuery } from "@mui/material";
+import { IconButton } from "@mui/material";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { DivHeader, MobileNav, ButtonHeader, Nav } from "./style";
+import { useNavigate } from 'react-router-dom';
+
+const RegisterHeader = () => {
+    const [menuAnchor, setMenuAnchor] = useState<null | HTMLElement>(null);
+
+    const navigate = useNavigate();
+
+    const handleMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setMenuAnchor(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setMenuAnchor(null);
+    };
+
+    const isMobile = useMediaQuery("(max-width: 700px)");
+
+    return (
+        <>
+            <DivHeader id="register_header">
+                <img src={LogoHeader} alt="Logo" />
+                {isMobile ? (
+                    <MobileNav>
+                        <IconButton onClick={handleMenuOpen}>
+                            <GiHamburgerMenu />
+                        </IconButton>
+                        <Menu
+                            anchorEl={menuAnchor}
+                            open={Boolean(menuAnchor)}
+                            onClose={handleMenuClose}
+                        >
+                            <ButtonHeader onClick={() => navigate('/')}>Ir para Home</ButtonHeader>
+                            <ButtonHeader onClick={() => navigate('/login')}>Fazer Login</ButtonHeader>
+                        </Menu>
+                    </MobileNav>
+                ) : (
+                    <Nav>
+                        <ButtonHeader onClick={() => navigate('/')}>Ir para Home</ButtonHeader>
+                        <ButtonHeader onClick={() => navigate('/login')}>Fazer Login</ButtonHeader>
+                    </Nav>
+                )}
+            </DivHeader>
+        </>
+    );
+};
+
+export default RegisterHeader;
---
+++ frontend/src/components/RegisterHeader/style.ts
@@ -0,0 +1,59 @@
+import { styled } from "styled-components";
+
+export const DivHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: .7rem;
+  
+  background: var(--white);
+  
+  @media (min-width: 768px) {
+    position: fixed;
+    top: 0;
+    right: 0;
+    z-index: 1;
+    
+    width: 100%;
+    padding: 1rem 3.75rem;
+
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+  }
+`;
+
+export const Nav = styled.nav`
+  display: flex;
+  gap: 1rem;
+`;
+
+export const ButtonHeader = styled.button`
+  background: none;
+  border-radius: 6px;
+  
+  cursor: pointer;
+  
+  padding: 10px 20px;
+
+  font-size: .7rem;
+  font-weight: 600;
+  color: var(--primary-color);
+
+  transition: .2s ease;
+
+  @media (min-width: 768px) {
+    border: 1px solid var(--primary-color);
+    font-size: .9rem;
+
+    &:hover {
+      border: 1px solid var(--black);
+      color: var(--black);
+    }
+  }
+`;
+
+export const MobileNav = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  align-items: flex-end;
+`;
