0f5cca044b199df55f6ceca25dc87e6f7ecfad29
Samuel-3004 <samuelszportela@hotmail.com>
2023-08-14 12:15:28 -0300

    feat: create CarProvider

--- frontend/src/providers/@types.ts
+++ frontend/src/providers/@types.ts
@@ -1,38 +1,43 @@
-import { ICreateUser } from '../components/RegisterForm/@types'
+import { ICreateUser } from "../components/RegisterForm/@types";
+import { ICar, TUserCarsResponse } from "./CarProvider/@types";
 
 export interface IUserContext {
-	user: IUser | null
-	loading: boolean
-	setLoading: React.Dispatch<React.SetStateAction<boolean>>
-	userLogin: (formData: ILogin) => Promise<void>
-	userRegister: (formData: ICreateUser) => Promise<void>
-	logout: () => void
+  user: IUser | null;
+  loading: boolean;
+  listCarsUser: [] | ICar[];
+  userIdCars: TUserCarsResponse | null;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  setListCarsUser: React.Dispatch<React.SetStateAction<[] | ICar[]>>;
+  setUserIdCars: React.Dispatch<React.SetStateAction<TUserCarsResponse | null>>;
+  userLogin: (formData: ILogin) => Promise<void>;
+  userRegister: (formData: ICreateUser) => Promise<void>;
+  logout: () => void;
 }
 
 export interface IDefaultProviderProps {
-	children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export interface IUser {
-	"id": string
-	"name": string,
-	"email": string,
-	"password": string,
-	"seller": boolean,
-	"cellPhone": string,
-	"cpf": string,
-	"dateOfBirth": string,
-	"description": string,
-	"city": string,
-	"state": string,
-	"street": string,
-	"number": number,
-	"complement": string
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  seller: boolean;
+  cellPhone: string;
+  cpf: string;
+  dateOfBirth: string;
+  description: string;
+  city: string;
+  state: string;
+  street: string;
+  number: number;
+  complement: string;
 }
 
-export interface IRegister extends Omit<IUser, 'id'> { }
+export interface IRegister extends Omit<IUser, "id"> {}
 
 export interface ILogin {
-	email: string
-	password: string
+  email: string;
+  password: string;
 }
