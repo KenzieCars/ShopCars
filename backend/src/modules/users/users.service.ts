f3acab4406c24f074743a82aa1b26d8db7cbd5fc
saviocosta01 <savio.costa.silva@gmail.com>
2023-08-15 17:47:04 -0300

    feat: create send email

--- backend/src/modules/users/users.service.ts
+++ backend/src/modules/users/users.service.ts
@@ -6,10 +6,12 @@ import {
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersRepository } from './repositories/users.repository';
+import { MailService } from '../utils/mail.service';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class UsersService {
-  constructor(private usersRepository: UsersRepository) {}
+  constructor(private usersRepository: UsersRepository, private mailService: MailService) {}
 
   async create(createUserDto: CreateUserDto) {
     const findUser = await this.usersRepository.findByEmail(
@@ -63,5 +65,34 @@ export class UsersService {
     }
 
     return this.usersRepository.delete(id);
+  }
+
+  async sendEmailResetPassword(email: string) {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new NotFoundException('User Not found');
+    }
+
+    const resetToken = randomUUID();
+
+    await this.usersRepository.updateToken(email, resetToken);
+
+    const resetPasswordTemplate = await this.mailService.resetPasswordTemplate(
+      email,
+      user.name,
+      resetToken,
+    );
+    await this.mailService.sendEmail(resetPasswordTemplate);
+  }
+
+  async resetPassword(password: string, resetToken: string) {
+    const user = await this.usersRepository.findByToken(resetToken);
+
+    if (!user) {
+      throw new NotFoundException('User Not found');
+    }
+
+    await this.usersRepository.updatePassword(user.id, password);
   }
 }
