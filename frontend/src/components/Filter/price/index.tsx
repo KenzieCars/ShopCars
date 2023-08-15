ad6fe4a416d4ccf8e381aa17ae7808d1384d3712
GedsonCS <gedsoncandido94@gmail.com>
2023-08-15 12:04:41 -0300

    para realizar pull

--- frontend/src/components/Filter/price/index.tsx
+++ frontend/src/components/Filter/price/index.tsx
@@ -10,7 +10,10 @@ const valuetext = (value: number) => {
 export const RangeSlider = () => {
   const { valueCar, setValueCar } = useContext(HomeContext);
 
-  const handleChange = (event: ChangeEvent<{}>, newValue: number | number[]) => {
+  const handleChange = (
+    event: ChangeEvent<{}>,
+    newValue: number | number[]
+  ) => {
     setValueCar(newValue as number[]);
   };
 
