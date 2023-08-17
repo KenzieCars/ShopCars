3e78a3933b9ba37e0ec59c77632ac55b9b2f5585
Samuel-3004 <samuelszportela@hotmail.com>
2023-08-17 14:56:12 -0300

    fix: correction conconflict correction

--- frontend/src/pages/ProductPage/index.tsx
+++ frontend/src/pages/ProductPage/index.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import { ProductMainContainer } from './style'
-import { HeaderUserPage } from '../../components/header'
+import React from "react";
+import { ProductMainContainer } from "./style";
+import { HeaderUserPage } from "../../components/Header";
 
 const ProductPage = () => {
   return (
     <ProductMainContainer>
       <HeaderUserPage />
       <figure>
-        <img src="https://www.usnews.com/object/image/00000182-a544-dc41-a1db-a56f118e0000/2a_2021_kia_telluride.jpg?update-time=&size=responsive640" alt="car image" />
+        <img
+          src="https://www.usnews.com/object/image/00000182-a544-dc41-a1db-a56f118e0000/2a_2021_kia_telluride.jpg?update-time=&size=responsive640"
+          alt="car image"
+        />
       </figure>
       <section>
         <h2>Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200</h2>
@@ -20,9 +23,8 @@ const ProductPage = () => {
         </div>
       </div>
       <button>Comprar</button>
-      
     </ProductMainContainer>
-  )
-}
+  );
+};
 
-export default ProductPage
+export default ProductPage;
