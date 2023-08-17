0e46577f6eb9e10cb03a7198f00049df0119d916
Fabio Luiz <fabioeeereed@gmail.com>
2023-08-17 16:57:44 -0300

    some changes at sectionProfileView adding logic

--- frontend/src/components/SectionProfileView/index.tsx
+++ frontend/src/components/SectionProfileView/index.tsx
@@ -3,7 +3,7 @@ import { ButtonCreate, SectionProfileInfoComponent, SiglaUser, UserDiv } from ".
 import { UserContext } from "../../providers/UserProvider/UserContext"
 
 const SectionProfileInfo = () => {
-  const { user, userIdCars } = useContext(UserContext)
+  const { userIdCars } = useContext(UserContext)
 
   return (
     <SectionProfileInfoComponent>
@@ -11,13 +11,11 @@ const SectionProfileInfo = () => {
         <SiglaUser>{userIdCars?.name[0]}</SiglaUser>
         <div>
           <span>{userIdCars?.name}</span>
-          <span>Anunciante</span>
+          {userIdCars?.seller === true ? <span>Anunciante</span> : <span>Comprador</span>}
         </div>
       </UserDiv>
-      {/* <p>{userIdCarsser?.description}</p> */}
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora magnam eligendi eius perferendis nesciunt cumque ab, accusantium ratione voluptatibus? Quasi quia distinctio cupiditate velit numquam officiis pariatur autem quae alias?</p>
-      <ButtonCreate>Criar anúncio</ButtonCreate>
-      {user?.isAdmin === true ? <ButtonCreate>Criar anúncio</ButtonCreate> : null }
+      <p>{userIdCars?.description}</p>
+      {userIdCars?.seller === true ? <ButtonCreate>Criar anúncio</ButtonCreate> : null }
     </SectionProfileInfoComponent>
   )
 }
