c9cba09b3b54aa873eb4a5f0a4fa3cbd1b8074a8
Daniel Willian <shay_dani@outlook.com>
2023-08-18 14:56:42 -0300

    feat: reset password front end and resolve more conflicts and resolve conflicts

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -15,7 +15,7 @@ import { HomeContext } from "../../providers/HomeProvider/HomeProvider";
 import { useContext } from "react";
 import ButtonHome from "../../components/Filter/ButonModal";
 import ModalFilter from "../../components/Filter/ModalRenderFilter";
-import { Header } from "../../components/Header";
+import { Header } from "../../components/header";
 
 // import { CarContext } from "../../providers/CarProvider/CarContext";
 
