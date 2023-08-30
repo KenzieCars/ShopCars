84f2077edf3a7869d4573bbb628c17e033793783
saviocosta01 <savio.costa.silva@gmail.com>
2023-08-30 14:12:26 -0300

    fix: delete User

--- frontend/src/providers/UserProvider/UserContext.tsx
+++ frontend/src/providers/UserProvider/UserContext.tsx
@@ -213,6 +213,7 @@ export const UserProvider = ({ children }: IDefaultProviderProps) => {
       });
 
       setUser(null);
+      setUserIdCars(null);
       localStorage.clear();
 
       toast.success("Conta deletada");
